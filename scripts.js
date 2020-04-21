acc928ccf776ea5c777744d52e4843e7da70086f
Davison Silva <47065921+davisonv@users.noreply.github.com>
2020-04-21 16:57:25 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,36 +1,25 @@
-/*function mostrarValor() {
-    alert(document.getElementById("top").value);
-}
-
-document.getElementById("top").onkeypress = function(e) {
-    // 13 é a tecla <ENTER>. Se ela for pressionada, mostrar o valor
-    if (e.keyCode == 13) {
-        mostrarValor();
-        e.preventDefault();
-    }
-}
-
-document.getElementById("button").onclick = function(e) {
-    mostrarValor();
-    e.preventDefault();
-}*/
-
+var root = document.documentElement;
 var form = document.getElementById('inputs');
 var campo1 = document.getElementById('top');
 var campo2 = document.getElementById('right');
 var campo3 = document.getElementById('bottom');
 var campo4 = document.getElementById('left');
 
-form.addEventListener('submit', function(e) {
-    // alerta o valor do campo
-    console.log(campo1.value);
 
+form.addEventListener('submit', function(e) {
     // impede o envio do form
     e.preventDefault();
+
+    root.style.setProperty('--border-radius', `${campo1.value}px`);
+    root.style.setProperty('--border-radius2', `${campo2.value}px`);
+    root.style.setProperty('--border-radius3', `${campo3.value}px`);
+    root.style.setProperty('--border-radius4', `${campo4.value}px`);
 });
 
 
-document.querySelector("box").style.borderRadius = campo1;
+
+
+
 
 
 
