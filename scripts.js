6d2c78b4cfc77d075be11e6893c20d9ff9f75620
Davison Silva <47065921+davisonv@users.noreply.github.com>
2020-04-26 01:08:53 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,17 +7,24 @@ var campo4 = document.getElementById('left');
 var empty  = document.getElementById('top')
 
 form.addEventListener('submit', function(e) {
-    // impede o envio do form
+    
     e.preventDefault();
 
     root.style.setProperty('--border-radius', `${campo1.value}px`);
     root.style.setProperty('--border-radius2', `${campo2.value}px`);
     root.style.setProperty('--border-radius3', `${campo3.value}px`);
     root.style.setProperty('--border-radius4', `${campo4.value}px`);
-    console.log(campo1.value)
+    
     document.getElementById("box-text").innerText = `border-radius: ${campo1.value}px ${campo2.value}px
-${campo3.value}px ${campo4.value}px`
+    ${campo3.value}px ${campo4.value}px;`
 
+   if(campo1.value !=0, campo2.value == 0, campo3.value == 0, campo4.value == 0) { 
+        var count = 2
+        while (count < 5) {  
+            root.style.setProperty(`--border-radius${count}`, `${campo1.value}px`)
+            count++
+        }
+    }
 });
 
 
