bbe99fc1428b707ac3eb81cd4325a0e53de002c3
Davison Silva <47065921+davisonv@users.noreply.github.com>
2020-04-28 02:05:06 -0300

    Add files via upload
    Criado Butão para copiar o resultado do codigo do border-radius
    Solucionada a necessidade de preencher todos os campos, agora se um campo for deixado em branco ele terá automaticamente valor 0

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
-//import { unicFillException } from './Exceptions'
-
 var root = document.documentElement;
 var form = document.getElementById('inputs');
 var campo1 = document.getElementById('top');
 var campo2 = document.getElementById('right');
 var campo3 = document.getElementById('bottom');
 var campo4 = document.getElementById('left');
-var empty  = document.getElementById('top') 
+var copy = document.getElementById('copy-btn');
+
 
 form.addEventListener('submit', function(e) {
     
@@ -17,22 +16,34 @@ form.addEventListener('submit', function(e) {
     root.style.setProperty('--border-radius3', `${campo3.value}px`);
     root.style.setProperty('--border-radius4', `${campo4.value}px`);
 
-
-
     unicFillException(campo1,campo2,campo3,campo4)
     
-   
-
+    emptyException(campo1,campo2,campo3,campo4)
     
+    var clipboard = `border-radius: ${campo1.value}px ${campo2.value}px ${campo3.value}px ${campo4.value}px;`
+    
+    document.getElementById("box-text").innerText = clipboard
 
-    document.getElementById("box-text").innerText = `border-radius: ${campo1.value}px ${campo2.value}px
-    ${campo3.value}px ${campo4.value}px;`
-});
+    copy.addEventListener("click", function(){
+        //Cria um elemento input (pode ser um textarea)
+        let inputTest = document.createElement("input")
+        inputTest.value = clipboard
+        //Anexa o elemento ao body
+        document.body.appendChild(inputTest)
+        //seleciona todo o texto do elemento
+        inputTest.select()
+        //executa o comando copy
+        //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+        document.execCommand('copy')
+        //remove o elemento
+        document.body.removeChild(inputTest)
+    })
+})
 
 
 
 
-function unicFillException (campo1,campo2,campo3,campo4) {
+function unicFillException (campo1,campo2,campo3,campo4){
     if(campo1.value !=0 & campo2.value == 0 & campo3.value == 0 & campo4.value == 0) { 
         var count = 2
         while (count < 5) {  
@@ -87,6 +98,27 @@ function unicFillException (campo1,campo2,campo3,campo4) {
     }
 }
 
+function emptyException (campo1,campo2,campo3,campo4){
+    if (campo1.value == 0){
+        campo1.value = 0
+        root.style.setProperty('--border-radius1', `${campo1.value}px`);
+    }
+
+    if (campo2.value == 0){
+        campo2.value = 0
+        root.style.setProperty('--border-radius2', `${campo2.value}px`);
+    }
+
+    if (campo3.value == 0){
+        campo3.value = 0
+        root.style.setProperty('--border-radius3', `${campo3.value}px`);
+    }
+
+    if (campo4.value == 0){
+        campo4.value = 0
+        root.style.setProperty('--border-radius4', `${campo4.value}px`);
+    }
+}
 
 
 
